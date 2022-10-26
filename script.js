c7a72f152fdce4274077b642e64e9b05e43b2ac2
Lashak111 <lkaraulashvili01@gmail.com>
2022-10-27 01:43:31 +0400

    footer

--- script.js
+++ script.js
@@ -1,29 +1,29 @@
 // burger
-const toggleNav = () => {
-  if (!menuOpen) {
-    Burgermenu.classList.add("open");
-    menuOpen = true;
+// const toggleNav = () => {
+//   if (!menuOpen) {
+//     Burgermenu.classList.add("open");
+//     menuOpen = true;
 
-    nav.classList.add("show");
-  } else {
-    nav.classList.remove("show");
-    Burgermenu.classList.remove("open");
-    menuOpen = false;
-  }
-};
+//     nav.classList.add("show");
+//   } else {
+//     nav.classList.remove("show");
+//     Burgermenu.classList.remove("open");
+//     menuOpen = false;
+//   }
+// };
 
-let Burgermenu = document.querySelector(".menu-btn");
+// let Burgermenu = document.querySelector(".menu-btn");
 
-let menuOpen = false;
-const nav = document.getElementById("nav-items");
+// let menuOpen = false;
+// const nav = document.getElementById("nav-items");
 
-Burgermenu.addEventListener("click", () => {
-  toggleNav();
-});
+// Burgermenu.addEventListener("click", () => {
+//   toggleNav();
+// });
 
-nav.addEventListener("click", () => {
-  toggleNav();
-});
+// nav.addEventListener("click", () => {
+//   toggleNav();
+// });
 
 
 
