7ce63a26e1888339369520143dbdc05eba31c83e
Lashak111 <lkaraulashvili01@gmail.com>
2022-10-27 00:01:18 +0400

    slider

--- script.js
+++ script.js
@@ -1,3 +1,33 @@
+// burger
+const toggleNav = () => {
+  if (!menuOpen) {
+    Burgermenu.classList.add("open");
+    menuOpen = true;
+
+    nav.classList.add("show");
+  } else {
+    nav.classList.remove("show");
+    Burgermenu.classList.remove("open");
+    menuOpen = false;
+  }
+};
+
+let Burgermenu = document.querySelector(".menu-btn");
+
+let menuOpen = false;
+const nav = document.getElementById("nav-items");
+
+Burgermenu.addEventListener("click", () => {
+  toggleNav();
+});
+
+nav.addEventListener("click", () => {
+  toggleNav();
+});
+
+
+
+
 // accordion
 let accordion = document.querySelectorAll(".acordion-box");
 
@@ -6,7 +36,7 @@ for (let item of accordion) {
     this.classList.toggle("active");
   });
 }
-
+// scroll
 let scroll = document.getElementById("Scrooltop");
 
 scroll.addEventListener("click", () => {
@@ -16,30 +46,35 @@ scroll.addEventListener("click", () => {
   });
 });
 
+// slider
 
 let slideIndex = 1;
 showSlides(slideIndex);
 
 function plusSlides(n) {
-  showSlides(slideIndex += n);
+  showSlides((slideIndex += n));
 }
 
 function currentSlide(n) {
-  showSlides(slideIndex = n);
+  showSlides((slideIndex = n));
 }
 
 function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("mySlides");
   let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = slides.length}
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
   for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
+    slides[i].style.display = "none";
   }
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
 }
